b1e24220e8f21ffb151a0091f5c5f5ba4f6ebf72
Diego <diegomourasilva88@gmail.com>
2026-04-05 18:39:03 -0300

    adiciona funçao de cadastro simples

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Diegite 3 numeros ai");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        if (num1 > num2 && num1 > num3){
-            System.out.println("esse numero e o maior" + num1);
-        } else if (num2 > num1 && num2 > num3) {
-            System.out.println("esse e o maior = " + num2);
-        }else
-            System.out.println(" esse e o maior numero = " + num3);
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Sua idade e? = ");
+         int idade = sc.nextInt();
+         System.out.println("Swu nome e ?");
+         String nome = sc.next();
+         System.out.println("prazer em te conhecer " + nome + " fico feliz em saber que vc tem " + idade);
+
 
     }
 }
